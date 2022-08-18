1195eb523e4b18b6d3b1d4fc3db6f6716c7a01c8
Pattan <patipan.polv@ku.th>
2022-08-18 16:52:28 +0700

    dd

--- DE/Adptive-DSIDE/DSIDE-Adaptivemutantselect.py
+++ DE/Adptive-DSIDE/DSIDE-Adaptivemutantselect.py
@@ -1,23 +1,24 @@
 import sys
-#main_path= 'C:/Users/patip/Documents/Python/Algorithm'
+# main_path= 'C:/Users/patip/OneDrive/Documents/python/Algorithm'
 #main_path = '/home/pythonrunnew/Algorithm'
 main_path = '/myweb/www/pythonrunnew/Algorithm'
 sys.path.insert(0,main_path)
-from AdaptiveDSIDE4 import DSIDEmedianVector
-from Benchmark.functionbenchmark import Sphere,Elliptic,Schwefel12,Schwefel221,Schwefel222,Zakharov
+from AdaptiveDSIDE7 import DSIDEmixVector
+from Benchmark.functionbenchmark import HGBat,Scaffer_F6,Scaffer2,HappyCat
+# from Benchmark.functionbenchmark import Scaffer2,Scaffer_F6,HappyCat,HGBat
 # from Benchmark.functionbenchmark import Bentcigar
 
-Sphere_obj = DSIDEmedianVector(functioneveluate=Sphere,functionname='Sphere',dimension=30,round=5000,populationsize=500)
-Elliptic_obj = DSIDEmedianVector(functioneveluate=Elliptic,functionname='Elliptic',dimension=30,round=5000,populationsize=500)
-Schwefel12_obj = DSIDEmedianVector(functioneveluate=Schwefel12,functionname='Schwefel12',dimension=30,round=5000,populationsize=500)
-Schwefel222_obj = DSIDEmedianVector(functioneveluate=Schwefel222,functionname='Schwefel222',dimension=30,round=5000,populationsize=500,upperbound=10,lowerbound=-10)
-Schwefel221_obj = DSIDEmedianVector(functioneveluate=Schwefel221,functionname='Schwefel221',dimension=30,round=5000,populationsize=500)
-Zakharov_obj = DSIDEmedianVector(functioneveluate=Zakharov,functionname='Zakharov',dimension=30,round=5000,populationsize=500,upperbound=10,lowerbound=-5)
+HGBat_obj = DSIDEmixVector(functioneveluate=HGBat,functionname='HGBat',dimension=30,round=5000,populationsize=100,functiontype='MultimodalFunction')
+Scaffer_F6_obj = DSIDEmixVector(functioneveluate=Scaffer_F6,functionname='Scaffer_F6',dimension=30,round=5000,populationsize=100,functiontype='MultimodalFunction')
+Scaffer2_obj = DSIDEmixVector(functioneveluate=Scaffer2,functionname='Scaffer2',dimension=30,round=5000,populationsize=100,functiontype='MultimodalFunction')
+# Schwefel222_obj = DSIDEmixVector(functioneveluate=Schwefel222,functionname='Schwefel222',dimension=30,round=5000,populationsize=100,functiontype='MultimodalFunction',upperbound=10,lowerbound=-10)
+HappyCat_obj = DSIDEmixVector(functioneveluate=HappyCat,functionname='HappyCat',dimension=30,round=5000,populationsize=100,functiontype='MultimodalFunction')
+# Zakharov_obj = DSIDEmixVector(functioneveluate=Zakharov,functionname='Zakharov',dimension=30,round=5000,populationsize=100,functiontype='MultimodalFunction',upperbound=10,lowerbound=-5)
 
-Sphere_obj.optimize(filename='DSIDEmedianVector500population')
-Elliptic_obj.optimize(filename='DSIDEmedianVector500population')
-Schwefel12_obj.optimize(filename='DSIDEmedianVector500population')
-Schwefel222_obj.optimize(filename='DSIDEmedianVector500population')
-Schwefel221_obj.optimize(filename='DSIDEmedianVector500population')
-Zakharov_obj.optimize(filename='DSIDEmedianVector500population')
+HGBat_obj.optimize(filename='DSIDEmixVectorpopulation')
+Scaffer_F6_obj.optimize(filename='DSIDEmixVectorpopulation')
+Scaffer2_obj.optimize(filename='DSIDEmixVectorpopulation')
+# Schwefel222_obj.optimize(filename='DSIDEmixVectorpopulation')
+HappyCat_obj.optimize(filename='DSIDEmixVectorpopulation')
+# Zakharov_obj.optimize(filename='DSIDEmixVectorpopulation')
 
